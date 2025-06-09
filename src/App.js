eba55971aa4aa065c56582f0bde66410f9c25836
amanda-bann <amanda.banner@gmail.com>
2025-06-09 17:48:20 -0600

    Assign data to components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "./Header";
 import NotesList from "./NotesList";
 
-const App = () => (
-  <div>
-    <Header />
-    <NotesList />
-  </div>
-);
+/* App component is the main component that holds the state of the application.
+   It contains the notes and the search text. It passes the notes and search text
+   to the NotesList component, which is responsible for rendering the list of notes.
+   The Header component is also rendered here.
+*/
+class App extends Component {
+
+  state = {
+    notes: [
+      {
+        id: 0,
+        title: "eat",
+        description: "reese peanut butter cups",
+        doesMatchSearch: true
+      },
+      {
+        id: 1,
+        title: "sleep",
+        description: "eight hours",
+        doesMatchSearch: true
+      },
+      {
+        id: 2,
+        title: "code",
+        description: "build an awesome ui",
+        doesMatchSearch: true
+      }
+    ],
+    searchText: "search"
+  };
+  render () {
+    return (
+      <div>
+        <Header />
+        <NotesList />
+      </div>
+    );
+  }
+};
 
 export default App;
