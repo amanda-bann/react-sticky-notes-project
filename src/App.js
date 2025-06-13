c53ccdb94243a97c103d8450f440e5161ac2c013
amanda-bann <amanda.banner@gmail.com>
2025-06-12 18:04:33 -0600

    Add a new note to the UI

--- src/App.js
+++ src/App.js
@@ -9,36 +9,34 @@ class App extends Component {
   state = {
     notes: [
       {
-        id: 0,
-        title: "eat",
-        description: "reese peanut butter cups",
-        doesMatchSearch: true
-      },
-      {
-        id: 1,
-        title: "sleep",
-        description: "eight hours",
-        doesMatchSearch: false
-      },
-      {
-        id: 2,
-        title: "code",
-        description: "build an awesome ui",
-        doesMatchSearch: true
-      },
-      {
-        id: 3,
-        title: "repeat",
-        description: "do it all over again",
-        doesMatchSearch: true
+      id: Date.now(),
+      title: "",
+      description: "",
+      doesMatchSearch: true,
       }
     ],
     searchText: "search"
   };
+
+  addNote = () => {
+    // Create a new note
+    const newNote = {
+      id: Date.now(),
+      title: "",
+      description: "",
+      doesMatchSearch: true,
+    };
+    // Add the new note to the existing notes array in State
+    this.setState({ notes: [newNote, ...this.state.notes]});
+    // Optionally, you could write it as follows:
+    // const newNotes = [newNote, ...this.state.notes];
+    // this.setState({ notes: newNotes });
+  };
+
   render () {
     return (
       <div>
-        <Header searchText={this.state.searchText} />
+        <Header addNote={this.addNote} searchText={this.state.searchText} />
         <NotesList notes={this.state.notes} />
       </div>
     );
